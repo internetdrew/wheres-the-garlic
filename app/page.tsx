b4e265d46b1f54b2a19524dca1f63b293edb9cbb
Andrew Rowley <andrew@internetdrew.com>
2025-03-08 11:30:53 -0500

    update heading

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
       <main className='mt-24'>
         <header className='flex flex-col gap-4 max-w-screen-sm mx-auto text-center'>
           <h1 className='text-3xl font-semibold lg:text-5xl'>
-            A Simple Home Grocery Inventory App
+            A Status-Based Home Grocery Inventory App
           </h1>
           <p className='px-8 text-lg text-neutral-400 lg:px-4 lg:text-xl'>
             See what’s in your kitchen at a glance—full, halfway, almost done,
