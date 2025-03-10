4af9d906569ddafbb093425f9664f6ffd4eaa74f
Andrew Rowley <andrew@internetdrew.com>
2025-03-10 12:36:37 -0400

    add main element and manage margins

--- app/(application)/dashboard/page.tsx
+++ app/(application)/dashboard/page.tsx
@@ -1,7 +1,20 @@
+import { createClient } from '@/utils/supabase/server';
 import React from 'react';
 
-const page = () => {
-  return <div>page</div>;
+const page = async () => {
+  const supabase = await createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  return (
+    <main className='max-w-screen-lg mx-4 lg:mx-auto'>
+      <h1 className='text-2xl font-bold'>
+        Hi, {user?.user_metadata.full_name}!
+      </h1>
+    </main>
+  );
 };
 
 export default page;
