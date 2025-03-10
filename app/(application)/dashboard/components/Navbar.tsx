d82e7dd1f17c7e8c094043e9c4ac683f10149a95
Andrew Rowley <andrew@internetdrew.com>
2025-03-10 12:32:02 -0400

    update nav item sizes

--- app/(application)/dashboard/components/Navbar.tsx
+++ app/(application)/dashboard/components/Navbar.tsx
@@ -14,12 +14,12 @@ const Navbar = () => {
 
   return (
     <nav className='flex justify-between items-center py-3 sticky top-0 bg-neutral-950 max-w-screen-lg mx-4 z-10 lg:mx-auto'>
-      <Link href='/' className='font-bold'>
+      <Link href='/' className='font-bold text-lg'>
         Where&apos;s the Garlic?!
       </Link>
 
       <button
-        className='bg-neutral-200 text-neutral-950 text-xs font-medium px-4 py-2 rounded-full'
+        className='bg-neutral-200 text-neutral-950 text-sm font-medium px-4 py-2 rounded-full'
         onClick={handleSignOut}
       >
         Sign Out
