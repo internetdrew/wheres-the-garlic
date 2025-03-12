c0e70ef50d49c3b33f93792056fa48d09823a09e
Andrew Rowley <andrew@internetdrew.com>
2025-03-12 11:58:53 -0400

    improve membership query

--- app/(application)/dashboard/page.tsx
+++ app/(application)/dashboard/page.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { createClient } from '@/utils/supabase/server';
-import { redirect } from 'next/navigation';
 import HouseholdFormTriggers from './components/HouseholdFormTriggers';
+import { redirect } from 'next/navigation';
+import Link from 'next/link';
 
 const Dashboard = async () => {
   const supabase = await createClient();
-
   const {
     data: { user },
   } = await supabase.auth.getUser();
@@ -16,14 +16,31 @@ const Dashboard = async () => {
 
   const { data: memberships } = await supabase
     .from('household_members')
-    .select(`id, household_id, role, household:households(id, name)`)
+    .select(
+      `
+      id,
+      household:household_id(
+        id,
+        title,
+        creator_id,
+        created_at,
+        updated_at
+      ),
+      member_role
+      `
+    )
     .eq('member_id', user.id);
+
+  console.log(memberships);
 
   return (
     <main className='max-w-screen-lg mx-4 mt-10 lg:mx-auto'>
       <h1 className='text-xl font-bold'>
         Hi, {user?.user_metadata.full_name}!
       </h1>
+      <p className='text-sm text-gray-400'>
+        You can create up to 3 households.
+      </p>
       <HouseholdFormTriggers />
       <div className='mt-6'>
         <h2 className='text-lg font-bold'>Households</h2>
@@ -35,11 +52,18 @@ const Dashboard = async () => {
           )}
         </div>
       </div>
-      <ul className='mt-4 border border-gray-800 border-dashed rounded-md p-6'>
+      <ul className='mt-4 flex flex-col gap-4 border border-gray-800 border-dashed rounded-md p-6 md:grid md:grid-cols-3'>
         {memberships?.map(membership => (
-          <li key={membership.id}>
-            <p>Household: {membership.household.name}</p>
-            <p>Role: {membership.role}</p>
+          <li
+            key={membership.id}
+            className='relative bg-gray-900 rounded-md p-4'
+          >
+            <Link href={`/households/${membership.household.id}`}>
+              <span className='absolute inset-0' />
+              {membership.household.title}
+            </Link>
+            <p>Household: {membership.id}</p>
+            <p>Role: {membership.member_role}</p>
           </li>
         ))}
       </ul>
