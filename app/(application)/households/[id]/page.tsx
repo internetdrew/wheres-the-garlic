3bba7ace0dfa65a57551a30394cb500b7a55e379
Andrew Rowley <andrew@internetdrew.com>
2025-03-13 12:53:46 -0400

    improve breakpoint

--- app/(application)/households/[id]/page.tsx
+++ app/(application)/households/[id]/page.tsx
@@ -69,7 +69,7 @@ const HouseholdPage = async ({
   }
 
   return (
-    <main className='max-w-screen-md px-4 mt-10 md:mx-auto lg:px-0'>
+    <main className='max-w-screen-md px-4 mt-10 mx-auto md:px-0'>
       <PageHeader household={household} />
       <section className='mt-10'>
         <p className=' text-neutral-500'>
