57771224274a887856ee320324951cba10d36ffa
Andrew Rowley <andrew@internetdrew.com>
2025-03-13 11:50:10 -0400

    adjust header

--- app/(application)/households/[id]/components/PageHeader.tsx
+++ app/(application)/households/[id]/components/PageHeader.tsx
@@ -17,31 +17,34 @@ const PageHeader = ({ household }: PageHeaderProps) => {
   };
 
   return (
-    <header className='flex items-start justify-between'>
-      <div>
-        <h1 className='text-xl font-bold lg:text-2xl'>
-          {household?.title} List
-        </h1>
-        <div className='flex flex-col mt-1 text-neutral-500 sm:flex-row sm:items-center sm:gap-2'>
-          <span>Created by</span>
-          <span className='flex items-center gap-1'>
-            <Image
-              src={household.creator.avatar_url}
-              alt={household.creator.full_name}
-              width={20}
-              height={20}
-              className='rounded-full'
-            />
-            {household?.creator.full_name}
-          </span>
+    <header>
+      <div className='flex items-start justify-between'>
+        <div>
+          <h1 className='text-xl font-bold lg:text-2xl'>
+            {household?.title} List
+          </h1>
+          <div className='flex flex-col mt-1 text-neutral-500 sm:flex-row sm:items-center sm:gap-2'>
+            <span>Created by</span>
+            <span className='flex items-center gap-1'>
+              <Image
+                src={household.creator.avatar_url}
+                alt={household.creator.full_name}
+                width={20}
+                height={20}
+                className='rounded-full'
+              />
+              {household?.creator.full_name}
+            </span>
+          </div>
         </div>
+
+        <button
+          className='text-sm bg-neutral-200 px-4 py-2 rounded-md font-medium text-neutral-900 cursor-pointer'
+          onClick={triggerAddItemFormDialog}
+        >
+          Add Item
+        </button>
       </div>
-      <button
-        className='text-sm bg-neutral-200 px-4 py-2 rounded-md font-medium text-neutral-900 cursor-pointer'
-        onClick={triggerAddItemFormDialog}
-      >
-        Add Item
-      </button>
 
       <AddItemFormDialog
         householdId={household.id}
