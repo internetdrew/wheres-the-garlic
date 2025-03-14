07931ee07ec5b1b4ef34e512ceaddcf94b934d08
Andrew Rowley <andrew@internetdrew.com>
2025-03-14 12:06:01 -0400

    update the query to get the latest post update

--- utils/supabase/queries.ts
+++ utils/supabase/queries.ts
@@ -26,3 +26,39 @@ export const getHouseholdByIdQuery = (
 };
 
 export type Household = QueryData<ReturnType<typeof getHouseholdByIdQuery>>;
+
+export const getHouseholdsByUserIdQuery = (
+  supabaseClient: SupabaseServerClientType,
+  userId: string
+) => {
+  return supabaseClient
+    .from('household_members')
+    .select(
+      `
+      id,
+      household:household_id(
+        id,
+        title,
+        creator:creator_id(
+          full_name,
+          avatar_url
+        ),
+        created_at,
+        latest_item:household_items(
+          last_updated_at
+        )
+      ),
+      member_role
+      `
+    )
+    .eq('member_id', userId)
+    .order('last_updated_at', {
+      referencedTable: 'household.household_items',
+      ascending: false,
+    })
+    .limit(1, { referencedTable: 'household.household_items' });
+};
+
+export type HouseholdsByUserId = QueryData<
+  ReturnType<typeof getHouseholdsByUserIdQuery>
+>;
