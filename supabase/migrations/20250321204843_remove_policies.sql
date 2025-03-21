ef77be54aa482123794108281e708e5a47501440
Andrew Rowley <andrew@internetdrew.com>
2025-03-21 16:49:09 -0400

    drop all rls policies to pick up speed

---
+++ supabase/migrations/20250321204843_remove_policies.sql
@@ -0,0 +1,17 @@
+drop policy "Anyone can view invite codes" on "public"."household_invites";
+
+drop policy "Only household creators can manage invites" on "public"."household_invites";
+
+drop policy "Users can delete items for their household" on "public"."household_items";
+
+drop policy "Users can insert items for their household" on "public"."household_items";
+
+drop policy "Users can update items for their household" on "public"."household_items";
+
+drop policy "Users can view items for their household" on "public"."household_items";
+
+drop policy "Enable read access for household members and creators" on "public"."household_members";
+
+drop policy "Public profiles are viewable by authenticated users" on "public"."profiles";
+
+
