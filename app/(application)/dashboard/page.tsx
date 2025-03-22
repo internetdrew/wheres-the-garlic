6671339dde53c31db82bce174da356f558eb9a5a
Andrew Rowley <andrew@internetdrew.com>
2025-03-22 06:33:08 -0400

    strip down dashboard page components

--- app/(application)/dashboard/page.tsx
+++ app/(application)/dashboard/page.tsx
@@ -1,55 +1,12 @@
 import React from 'react';
-import { createClient } from '@/utils/supabase/server';
-import HouseholdFormTriggers from './components/HouseholdFormTriggers';
-import { redirect } from 'next/navigation';
-import { getHouseholdsByUserIdQuery } from '@/utils/supabase/queries';
-import HouseholdCard from './components/HouseholdCard';
+import MembershipList from './components/MembershipList';
+import DashboardHeader from './components/DashboardHeader';
 
 const Dashboard = async () => {
-  const supabase = await createClient();
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-
-  if (!user) {
-    redirect('/');
-  }
-
-  const { data: memberships } = await getHouseholdsByUserIdQuery(
-    supabase,
-    user.id
-  );
-
   return (
     <main className='max-w-screen-lg mx-4 mt-10 lg:mx-auto'>
-      <h1 className='text-xl font-bold'>
-        Hi, {user?.user_metadata.full_name}!
-      </h1>
-      <p className='text-sm text-neutral-500'>
-        You can create up to 3 households.
-      </p>
-      <HouseholdFormTriggers householdCount={memberships?.length || 0} />
-      <div className='mt-6'>
-        <h2 className='text-lg font-bold'>Households</h2>
-        <div className='text-neutral-500'>
-          {memberships?.length === 0 ? (
-            <p>You&apos;re not part of any households yet!</p>
-          ) : (
-            <p>You&apos;re part of the following households:</p>
-          )}
-        </div>
-      </div>
-      <ul
-        className={`my-4 flex flex-col gap-4  ${
-          memberships?.length === 0
-            ? 'border border-neutral-800 border-dashed rounded-md p-6'
-            : ''
-        } sm:grid sm:grid-cols-2 md:grid-cols-3`}
-      >
-        {memberships?.map(membership => (
-          <HouseholdCard key={membership.id} household={membership.household} />
-        ))}
-      </ul>
+      <DashboardHeader />
+      <MembershipList />
     </main>
   );
 };
