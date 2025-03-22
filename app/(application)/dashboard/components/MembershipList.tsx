a27cf689dfbcbc2939295ed23e6df6d9b27b47ed
Andrew Rowley <andrew@internetdrew.com>
2025-03-22 09:00:55 -0400

    ad loading state for cards

--- app/(application)/dashboard/components/MembershipList.tsx
+++ app/(application)/dashboard/components/MembershipList.tsx
@@ -1,13 +1,27 @@
 'use client';
 
 import HouseholdCard from './HouseholdCard';
-import { useMembershipList } from '@/app/hooks/useMembershipList';
+import { useHouseholdMemberships } from '@/app/hooks/useHouseholdMemberships';
 
 export default function MembershipList() {
-  const { memberships, isLoading, isError } = useMembershipList();
+  const { memberships, membershipsLoading, membershipsError } =
+    useHouseholdMemberships();
 
-  if (isLoading) return <div>Loading...</div>;
-  if (isError) return <div>Something broke here</div>;
+  if (membershipsLoading) {
+    return (
+      <ul className='flex flex-col gap-4 sm:grid sm:grid-cols-2 md:grid-cols-3'>
+        {Array.from({ length: 3 }).map((_, index) => (
+          <li
+            key={index}
+            className='my-4 w-full h-36 bg-neutral-800 animate-pulse rounded-md'
+          ></li>
+        ))}
+      </ul>
+    );
+  }
+  if (membershipsError) {
+    return <div>Error loading households. Please try again.</div>;
+  }
 
   return (
     <ul
