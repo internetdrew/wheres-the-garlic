b82714faf1a34effc1a31136098080161489571b
Andrew Rowley <andrew@internetdrew.com>
2025-03-22 09:00:29 -0400

    improve composition

--- app/(application)/dashboard/components/Navbar.tsx
+++ app/(application)/dashboard/components/Navbar.tsx
@@ -1,29 +1,14 @@
-'use client';
-
 import React from 'react';
 import Link from 'next/link';
-import { createClient } from '@/utils/supabase/client';
-import { redirect } from 'next/navigation';
-const Navbar = () => {
-  const supabase = createClient();
+import NavControls from './NavControls';
 
-  const handleSignOut = async () => {
-    await supabase.auth.signOut();
-    redirect('/');
-  };
-
+const Navbar = async () => {
   return (
     <nav className='flex justify-between items-center py-3 sticky top-0 bg-neutral-950 max-w-screen-lg mx-4 z-10 lg:mx-auto'>
-      <Link href='/' className='font-bold text-lg'>
+      <Link href='/' className='font-bold lg:text-lg'>
         Where&apos;s the Garlic?!
       </Link>
-
-      <button
-        className='bg-neutral-200 text-neutral-950 text-sm font-medium px-4 py-2 rounded-full cursor-pointer hover:bg-neutral-300'
-        onClick={handleSignOut}
-      >
-        Sign Out
-      </button>
+      <NavControls />
     </nav>
   );
 };
