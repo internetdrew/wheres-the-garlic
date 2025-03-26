fc19192e92f22091b5418d7e1ccc0efafbb649b6
Andrew Rowley <andrew@internetdrew.com>
2025-03-26 14:41:02 -0400

    add components for invites

--- app/(application)/dashboard/components/MembershipList.tsx
+++ app/(application)/dashboard/components/MembershipList.tsx
@@ -1,11 +1,18 @@
 'use client';
 
+import { useRef, useState } from 'react';
 import HouseholdCard from './HouseholdCard';
 import { useHouseholdMemberships } from '@/app/hooks/useHouseholdMemberships';
+import { HouseholdsByUserId } from '@/utils/supabase/queries';
+import HouseholdInviteDialog from './HouseholdInviteDialog';
 
 export default function MembershipList() {
+  const [chosenHousehold, setChosenHousehold] = useState<
+    HouseholdsByUserId[number]['household'] | null
+  >(null);
   const { memberships, membershipsLoading, membershipsError } =
     useHouseholdMemberships();
+  const dialogRef = useRef<HTMLDialogElement>(null);
 
   if (membershipsLoading) {
     return (
@@ -23,17 +30,34 @@ export default function MembershipList() {
     return <div>Error loading households. Please try again.</div>;
   }
 
+  const handleInviteClick = (
+    household: HouseholdsByUserId[number]['household']
+  ) => {
+    setChosenHousehold(household);
+    dialogRef.current?.showModal();
+  };
+
   return (
-    <ul
-      className={`my-4 flex flex-col gap-4  ${
-        memberships?.length === 0
-          ? 'border border-neutral-800 border-dashed rounded-md p-6'
-          : ''
-      } sm:grid sm:grid-cols-2 md:grid-cols-3`}
-    >
-      {memberships?.map(membership => (
-        <HouseholdCard key={membership.id} household={membership.household} />
-      ))}
-    </ul>
+    <>
+      <ul
+        className={`my-4 flex flex-col gap-4  ${
+          memberships?.length === 0
+            ? 'border border-neutral-800 border-dashed rounded-md p-6'
+            : ''
+        } sm:grid sm:grid-cols-2 md:grid-cols-3`}
+      >
+        {memberships?.map(membership => (
+          <HouseholdCard
+            key={membership.id}
+            household={membership.household}
+            onInviteClick={() => handleInviteClick(membership.household)}
+          />
+        ))}
+      </ul>
+      <HouseholdInviteDialog
+        dialogRef={dialogRef}
+        chosenHousehold={chosenHousehold}
+      />
+    </>
   );
 }
