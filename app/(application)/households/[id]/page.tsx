7127cbd2ca68b4231aa1ab3380a50938f17eb217
Andrew Rowley <andrew@internetdrew.com>
2025-03-26 09:11:50 -0400

    pass ids to page components

--- app/(application)/households/[id]/page.tsx
+++ app/(application)/households/[id]/page.tsx
@@ -1,7 +1,4 @@
-import { redirect } from 'next/navigation';
 import PageHeader from './components/PageHeader';
-import { createClient } from '@/utils/supabase/server';
-import { getHouseholdByIdQuery } from '@/utils/supabase/queries';
 import ItemList from './components/ItemList';
 
 const HouseholdPage = async ({
@@ -10,26 +7,11 @@ const HouseholdPage = async ({
   params: Promise<{ id: string }>;
 }) => {
   const { id } = await params;
-  const supabase = await createClient();
-
-  const { data: household, error: householdError } =
-    await getHouseholdByIdQuery(supabase, id);
-
-  if (householdError) {
-    console.error(householdError);
-  }
-
-  if (!household) {
-    redirect('/dashboard');
-  }
 
   return (
     <main className='max-w-screen-md px-4 mt-10 mx-auto md:px-0'>
-      <PageHeader
-        household={household}
-        itemsCount={household.items.length ?? 0}
-      />
-      <ItemList items={household.items ?? []} householdId={id} />
+      <PageHeader householdId={id} />
+      <ItemList householdId={id} />
     </main>
   );
 };
